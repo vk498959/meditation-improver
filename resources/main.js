ce3e74cf6bbed306cbd7833554edbd567a62e933
vikas <63574401+vk498959@users.noreply.github.com>
2022-07-26 12:03:14 +0530

    Add files via upload

---
+++ resources/main.js
@@ -0,0 +1,28 @@
+const start = document.getElementById("start");
+const center = document.querySelector("center");
+const ffullscreen = document.getElementById("fullscreen");
+
+var showtime = document.getElementById("time");
+var div1 = document.getElementById("div1");
+var time = 300;
+ffullscreen.addEventListener("click", () => {
+    if (center.fullscreenEnabled) {
+        document.exitFullscreen();
+        console.log("hello");
+    }
+    else {
+        center.requestFullscreen();
+    }
+
+    start.addEventListener("click", () => {
+
+
+    });
+    setInterval(() => {
+        showtime.innerHTML = time + " Second left";
+        if (time < 0)
+            div1.innerHTML = "Time out";
+        else
+            time--;
+    }, 1000);
+});
