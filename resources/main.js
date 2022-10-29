5c8dd455d4ddedaf84da21e877194638f6c00f4c
Vikas Kumar <63574401+vk498959@users.noreply.github.com>
2022-10-30 00:10:34 +0530

    Update main.js

--- resources/main.js
+++ resources/main.js
@@ -4,6 +4,7 @@ const ffullscreen = document.getElementById("fullscreen");
 
 var showtime = document.getElementById("time");
 var div1 = document.getElementById("div1");
+div1.style.display = "none";
 var time = 300;
 ffullscreen.addEventListener("click", () => {
     if (center.fullscreenEnabled) {
@@ -12,16 +13,23 @@ ffullscreen.addEventListener("click", () => {
     else {
         center.requestFullscreen();
     }
+});
+var interval = "";
+start.addEventListener("click", () => {
+    if (start.innerText == "Click to stop") {
+        clearInterval(interval);
+        div1.style.display = "none";
+        start.innerText = "Click to start";
+    } else {
+        div1.style.display = "block";
+        start.innerText = "Click to stop";
+        interval = setInterval(() => {
+            showtime.innerHTML = time + " Second left";
+            if (time < 0)
+                div1.innerHTML = "Time out";
+            else
+                time--;
+        }, 1000);
+    }
 
-    start.addEventListener("click", () => {
-
-
-    });
-    setInterval(() => {
-        showtime.innerHTML = time + " Second left";
-        if (time < 0)
-            div1.innerHTML = "Time out";
-        else
-            time--;
-    }, 1000);
 });
